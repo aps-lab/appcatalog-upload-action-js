771613905e2f717e2e802fe1cec37081740ddaa7
STH <sth@adorsys.de>
2020-05-22 15:51:05 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -367,11 +367,11 @@ module.exports = _head;
 
 const core = __webpack_require__(310);
 const shell = __webpack_require__(16)
-shell.config.fatal = true
+// shell.config.fatal = true
 shell.config.verbose = true
 
-// most @actions toolkit packages have async methods
-async function run() {
+// // most @actions toolkit packages have async methods
+// async function run() {
   try { 
       //inputs 
       const tenantId = core.getInput('tenantId')
@@ -381,18 +381,18 @@ async function run() {
 
       //checkout fastlane custom actions  
       // shell.exec('git clone ${FASTLANE_CUSTOM_ACTIONS_GIT_URL}'
-      console.log(process.env.FASTLANE_CUSTOM_ACTIONS_GIT_URL)
-      shell.exec(`git clone ${shell.env.FASTLANE_CUSTOM_ACTIONS_GIT_URL}`)
+
+      shell.exec('git clone ${FASTLANE_CUSTOM_ACTIONS_GIT_URL}')
       shell.cd('csi-fastlane-custom-actions/fastlane')
-      // shell.exec(`fastlane run distribute_to_appcatalog ktb_environment:'adorsys' tenant_id:${number}  appcatalog_app_id:${appId} file_path:${filePath} release_notes:${releaseNotes} `)
+      shell.exec(`fastlane run distribute_to_appcatalog ktb_environment:'adorsys' tenant_id:${number}  appcatalog_app_id:${appId} file_path:${filePath} release_notes:${releaseNotes} `)
   } 
   catch (error) {
     console.log(error)
     core.setFailed(error.message);
   }
-}
+// }
 
-run()
+// run()
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core');
 const shell = require('shelljs')
-shell.config.fatal = true
+// shell.config.fatal = true
 shell.config.verbose = true
 
-// most @actions toolkit packages have async methods
-async function run() {
+// // most @actions toolkit packages have async methods
+// async function run() {
   try { 
       //inputs 
       const tenantId = core.getInput('tenantId')
@@ -14,14 +14,16 @@ async function run() {
 
       //checkout fastlane custom actions  
       // shell.exec('git clone ${FASTLANE_CUSTOM_ACTIONS_GIT_URL}'
-      shell.exec(`git clone ${shell.env.FASTLANE_CUSTOM_ACTIONS_GIT_URL}`)
-      shell.cd('csi-fastlane-custom-actions/fastlane')
+      const git = process.env.FASTLANE_CUSTOM_ACTIONS_GIT_URL
+      shell.exec(`git clone ${git}`)
+      // shell.cd('csi-fastlane-custom-actions/fastlane')
       // shell.exec(`fastlane run distribute_to_appcatalog ktb_environment:'adorsys' tenant_id:${number}  appcatalog_app_id:${appId} file_path:${filePath} release_notes:${releaseNotes} `)
   } 
   catch (error) {
+    console.log("ERRROORRRR")
     console.log(error)
     core.setFailed(error.message);
   }
-}
+// }
 
-run()
+// run()
