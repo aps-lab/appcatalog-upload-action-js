bb3c2ba7b7d9bc6d9d64f6ce05a4e02bbb9f4456
Johannes Steib <jst@adorsys.de>
2020-05-26 09:19:03 +0200

    Install Fastlane if needed

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
 const core = require('@actions/core');
-const shell = require('shelljs')
-shell.config.fatal = true
+const shell = require('shelljs');
+const fs = require('fs');
+shell.config.fatal = true;
 
-try {
-  //inputs 
-  const tenantId = core.getInput('tenantId')
-  const appId = core.getInput('appId')
-  const filePath = core.getInput('filePath')
-  const releaseNotes = core.getInput('releaseNotes')
-  const path = '../../' + filePath
+function run() {
+  try {
+    //inputs 
+    const tenantId = core.getInput('tenantId');
+    const appId = core.getInput('appId');
+    const filePath = core.getInput('filePath');
+    const releaseNotes = core.getInput('releaseNotes');
+    const path = process.cwd() + '/' + filePath;
+  
+    //checkout fastlane custom actions  
+    const git = process.env.FASTLANE_CUSTOM_ACTIONS_GIT_URL;
+    shell.exec(`git clone ${git}`);
+  
+    //install fastlane if needed
+    let fastlaneCommand;
+    const gemfilePath = process.cwd() + '/Gemfile'
+    if (fs.existsSync(gemfilePath)) {
+      fastlaneCommand = 'bundle exec fastlane'
 
-  //checkout fastlane custom actions  
-  const git = process.env.FASTLANE_CUSTOM_ACTIONS_GIT_URL
-  shell.exec(`git clone ${git}`)
+      shell.config.fatal = false;
+      const result = shell.exec('bundle show fastlane');
+      if (result.code !== 0) {
+        shell.exec('bundle add fastlane')
+      }
+      shell.config.fatal = true;
+      
+      shell.exec('bundle install')
+    } else {
+      fastlaneCommand = 'fastlane'
 
-  //upload to appcatalog
-  shell.cd('csi-fastlane-custom-actions/fastlane')
-  shell.exec(`bundle exec fastlane run distribute_to_appcatalog ktb_environment:'adorsys' tenant_id:${tenantId}  appcatalog_app_id:${appId} file_path:${path} release_notes:${releaseNotes} `)
+      shell.exec('sudo gem install fastlane')
+    }
 
-  //reset current folder to home
-  shell.cd()
+    //upload to appcatalog
+    shell.cd('csi-fastlane-custom-actions/fastlane');
+    shell.exec(`${fastlaneCommand} run distribute_to_appcatalog ktb_environment:'adorsys' tenant_id:${tenantId}  appcatalog_app_id:${appId} file_path:${path} release_notes:${releaseNotes} `);
+  
+    //reset current folder to home
+    shell.cd();
+  } catch (error) {
+    setFailed(error)
+  }
 }
-catch (error) {
-  console.log(error)
+
+function setFailed(error) {
+  core.error(error);
   core.setFailed(error.message);
 }
+
+run();
